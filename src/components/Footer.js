304e40c8a20979f9e3812c79b5fceac3f8636fef
Mateus <mateuskvasconcelos@gmail.com>
2023-02-01 18:58:18 -0300

    Feat: Personal data insert in the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,28 @@ import React from "react";
 import styled from "styled-components";
 
 export default function Footer() {
-  return <FooterStyle>Footer</FooterStyle>;
+  return (
+    <FooterStyle>
+      <a href="https://github.com/mkvasconcelos">
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
+          alt="github logo"
+        />
+      </a>
+      <a href="https://www.linkedin.com/in/mateuskavamotovasconcelos/">
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/174/174857.png"
+          alt="github logo"
+        />
+      </a>
+      <a href="mailto: mateuskvasconcelos@gmail.com">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Gmail_icon_%282020%29.svg/2560px-Gmail_icon_%282020%29.svg.png"
+          alt="github logo"
+        />
+      </a>
+    </FooterStyle>
+  );
 }
 
 const FooterStyle = styled.footer`
@@ -16,4 +37,8 @@ const FooterStyle = styled.footer`
   align-items: center;
   color: white;
   background-color: #292826;
+  img {
+    width: 30px;
+    cursor: pointer;
+  }
 `;
